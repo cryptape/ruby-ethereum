0a4efa869d0c6c32964b181d6043e49fce0d14ba
Jan Xie <jan.h.xie@gmail.com>
2016-02-05 14:14:41 +0800

    keccak_rlp helper

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ A Ruby implementation of [Ethereum](https://ethereum.org).
 ## License
 
 [MIT License](LICENSE)
+
+## TODO
+
+* upgrade rlp gem dependency
--- lib/ethereum/utils.rb
+++ lib/ethereum/utils.rb
@@ -3,8 +3,14 @@ require 'digest/sha3'
 module Ethereum
   module Utils
 
+    extend self
+
     def keccak_256(x)
       Digest::SHA3.new(256).digest(x)
+    end
+
+    def keccak_rlp(x)
+      keccak_256 RLP.encode(x)
     end
 
   end
