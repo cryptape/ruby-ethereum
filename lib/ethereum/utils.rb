3b5757b1c1be03dbc3923deee3679a96dac7fc82
Jan Xie <jan.h.xie@gmail.com>
2016-02-06 21:52:32 +0800

    str_to_nibbles

--- lib/ethereum/utils.rb
+++ lib/ethereum/utils.rb
@@ -5,6 +5,8 @@ module Ethereum
 
     extend self
 
+    HEX_VALUES = (0..15).inject({}) {|h, i| h[i.to_s(16).b] = i; h}.freeze
+
     def keccak_256(x)
       Digest::SHA3.new(256).digest(x)
     end
@@ -13,5 +15,22 @@ module Ethereum
       keccak_256 RLP.encode(x)
     end
 
+    ##
+    # convert string s to nibbles (half-bytes)
+    #
+    # @example
+    #   str_to_nibbles('') # => []
+    #   str_to_nibbles('h') # => [6, 8]
+    #   str_to_nibbles('he') # => [6, 8, 6, 5]
+    #   str_to_nibbles('hello') # => [6, 8, 6, 5, 6, 12, 6, 12, 6, 15]
+    #
+    # @param s [String] any string
+    #
+    # @return [Array[Integer]] array of nibbles presented as integer smaller
+    #   than 16
+    #
+    def str_to_nibbles(s)
+      RLP::Utils.encode_hex(s).each_char.map {|nibble| HEX_VALUES[nibble] }
+    end
   end
 end
