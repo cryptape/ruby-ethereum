857335ae27ad4f8bbf067f9633870cabeba39256
Jan Xie <jan.h.xie@gmail.com>
2016-02-21 22:56:00 +0800

    receipt

---
+++ lib/ethereum/receipt.rb
@@ -0,0 +1,25 @@
+module Ethereum
+  class Receipt
+    include RLP::Sedes::Serializable
+
+    extend Sedes
+
+    set_serializable_fields(
+      state_root: trie_root,
+      gas_used: big_endian_int,
+      bloom: int256,
+      logs: RLP::Sedes::CountableList.new(Log)
+    )
+
+    def initialize(state_root, gas_used, logs, bloom=nil)
+      serializable_initialize(state_root, gas_used, nil, logs)
+      raise ArgumentError, "Invalid bloom filter" if bloom && bloom != self.bloom
+    end
+
+    def bloom
+      bloomables = logs.map {|l| l.bloomables }
+      Bloom.from_array bloomables.flatten
+    end
+
+  end
+end
