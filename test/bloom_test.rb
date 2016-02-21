1d30a5390b9c28e52ecf30980fb51d65f8fb3b94
Jan Xie <jan.h.xie@gmail.com>
2016-02-21 22:54:25 +0800

    bloom fixture tests

--- test/bloom_test.rb
+++ test/bloom_test.rb
@@ -14,3 +14,44 @@ class BloomTest < Minitest::Test
   end
 
 end
+
+class BloomFixtureTest < Minitest::Test
+  include Ethereum
+
+  run_fixtures "VMTests"
+
+  ##
+  # The logs sections is a mapping between the blooms and their corresponding
+  # log entries. Each log entry has the format:
+  #
+  # * address: the address of the log entry
+  # * data: the data of the log entry
+  # * topics: the topics of the log entry, given as an array of values
+  #
+  def on_fixture_test(name, testdata)
+    test_logs = testdata['logs']
+    if test_logs && name =~ /log/i
+      test_logs.each do |data|
+        address = Utils.decode_hex data['address']
+        b = Bloom.from address
+        data['topics'].each {|t| b = Bloom.insert(b, Utils.decode_hex(t)) }
+
+        topics = data['topics'].map {|t| decode_int_from_hex(t) }
+        log = Log.new address, topics, ''
+        log_bloom = Bloom.b256 Bloom.from_array(log.bloomables)
+
+        assert_equal Utils.encode_hex(log_bloom), encode_hex_from_int(b)
+        assert_equal data['bloom'].b, Utils.encode_hex(log_bloom)
+      end
+    end
+  end
+
+  def encode_hex_from_int(x)
+    Utils.encode_hex Utils.zpad(Utils.int_to_big_endian(x), 256)
+  end
+
+  def decode_int_from_hex(x)
+    Utils.decode_int Utils.decode_hex(x).sub(/^(\x00)+/, '')
+  end
+
+end
