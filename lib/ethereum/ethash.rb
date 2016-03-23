a145650c4bbb6f07d5504866a28cfbdff3051a3f
Jan Xie <jan.h.xie@gmail.com>
2016-03-23 11:32:56 +0800

    fix ehash, don't repeat cache generation for blocks in the same epoch

--- lib/ethereum/ethash.rb
+++ lib/ethereum/ethash.rb
@@ -6,9 +6,66 @@ require 'ethereum/ethash_ruby'
 module Ethereum
   module Ethash
 
+    EPOCH_LENGTH = ::Ethash::EPOCH_LENGTH
+
+    CACHE_BY_SEED_MAX = 32
+
     class <<self
+      def seeds
+        @seeds ||= ["\x00"*32]
+      end
+
+      def get_seed(block_number)
+        epoch_no = block_number / EPOCH_LENGTH
+        while seeds.size <= epoch_no
+          seeds.push Ethereum::Utils.keccak256(seeds.last)
+        end
+
+        seeds[epoch_no]
+      end
+
+      def cache_by_seed
+        @cache_by_seed ||= {} # ordered hash
+      end
+
+      def cache_by_file(block_number, data=nil)
+        path = "/tmp/ruby_ethereum_hashimoto_cache_#{block_number}"
+        if data
+          File.open(path, 'wb') {|f| f.write Marshal.dump(data) }
+        else
+          if File.exist?(path)
+            File.open(path, 'rb') {|f| Marshal.load f.read }
+          else
+            nil
+          end
+        end
+      end
+
       def get_cache(blknum)
-        ::Ethash.mkcache_bytes blknum
+        seed = get_seed blknum
+
+        if cache_by_seed.has_key?(seed)
+          c = cache_by_seed.delete seed # pop
+          cache_by_seed[seed] = c # and append at end
+          return c
+        end
+
+        if c = cache_by_file(Utils.encode_hex(seed))
+          cache_by_seed[seed] = c
+          return c
+        end
+
+        # Use c++ implementation or ethash_ruby
+        c = ::Ethash.mkcache_bytes blknum
+        #c = EthashRuby::Cache.new(blknum).to_a
+
+        cache_by_seed[seed] = c
+        cache_by_file Utils.encode_hex(seed), c
+        if cache_by_seed.size > CACHE_BY_SEED_MAX
+          cache_by_seed.delete cache_by_seed.keys.first # remove last recently accessed
+        end
+
+        c
       end
 
       def hashimoto_light(blknum, cache, mining_hash, bin_nonce)
--- lib/ethereum/ethash_ruby/cache.rb
+++ lib/ethereum/ethash_ruby/cache.rb
@@ -8,64 +8,6 @@ module Ethereum
     class Cache
       include Utils
 
-      CACHE_BY_SEED_MAX = 10
-
-      class <<self
-        def seeds
-          @seeds ||= ["\x00"*32]
-        end
-
-        def cache_by_seed
-          @cache_by_seed ||= {} # ordered hash
-        end
-
-        def cache_by_file(block_number, data=nil)
-          path = "/tmp/ruby_ethereum_hashimoto_cache_#{block_number}"
-          if data
-            File.open(path, 'wb') {|f| f.write Marshal.dump(data) }
-          else
-            if File.exist?(path)
-              File.open(path, 'rb') {|f| Marshal.load f.read }
-            else
-              nil
-            end
-          end
-        end
-
-        def get_seed(block_number)
-          epoch_no = block_number / EPOCH_LENGTH
-          while seeds.size <= epoch_no
-            seeds.push Ethereum::Utils.keccak256(seeds.last)
-          end
-
-          seeds[epoch_no]
-        end
-
-        def get(block_number)
-          seed = get_seed block_number
-
-          if cache_by_seed.has_key?(seed)
-            c = cache_by_seed.delete seed # pop
-            cache_by_seed[seed] = c # and append at end
-            return c
-          end
-
-          if c = cache_by_file(block_number)
-            cache_by_seed[seed] = c
-            return c
-          end
-
-          new(block_number).to_a.tap do |c|
-            cache_by_seed[seed] = c
-            cache_by_file block_number, c
-            if cache_by_seed.size > CACHE_BY_SEED_MAX
-              cache_by_seed.delete cache_by_seed.keys.first # remove last recently accessed
-            end
-          end
-        end
-        lru_cache :get, 16
-      end
-
       def initialize(block_number)
         @block_number = block_number
       end
