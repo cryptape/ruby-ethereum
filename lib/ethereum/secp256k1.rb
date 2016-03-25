c8a6418137610a6343a5638b132491d395977ead
Jan Xie <jan.h.xie@gmail.com>
2016-03-25 14:02:08 +0800

    cleanup

--- lib/ethereum/secp256k1.rb
+++ lib/ethereum/secp256k1.rb
@@ -2,7 +2,6 @@
 
 require 'base64'
 require 'secp256k1'
-require 'ethereum/ffi/openssl'
 
 module Ethereum
   module Secp256k1
@@ -53,57 +52,6 @@ module Ethereum
         recsig = pk.ecdsa_recoverable_deserialize(sig, vrs[0]-27)
         pk.public_key = pk.ecdsa_recover msg, recsig, raw: true
         pk.serialize compressed: compressed
-      end
-
-      def ecdsa_raw_sign(msghash, priv, compact=false)
-        pk = Secp256k1::PrivateKey.new
-
-        sig = pk.ecdsa_sign_recoverable Utils.zpad(msghash, 32)
-
-        v = Utils.big_endian_to_int sig[0]
-        r = Utils.big_endian_to_int sig[1,32]
-        s = Utils.big_endian_to_int sig[33,32]
-
-        raise InvalidPrivateKey, "invalid private key: #{priv.inspect}" if r == 0 && s == 0
-
-        [v,r,s]
-      end
-
-      def ecdsa_raw_verify(msghash, vrs, pub)
-        v, r, s = vrs
-        return false if v < 27 || v > 34
-
-        sig = ecdsa_sig_serialize r, s
-        verify(Utils.zpad(msghash, 32), sig, pub)
-      end
-
-      def ecdsa_raw_recover(msghash, vrs)
-        sig = encode_signature *vrs, false
-        raw = recover_compact Utils.zpad(msghash, 32), sig
-        PublicKey.new(raw).value
-      end
-
-      # static int secp256k1_ecdsa_sig_serialize(
-      #   unsigned char *sig, int *size, const # secp256k1_ecdsa_sig_t *a
-      # )
-      def ecdsa_sig_serialize(r, s)
-        len = (4 + r.size + s.size).chr
-        len_r = r.size.chr
-        len_s = s.size.chr
-        bytes_r = Utils.int_to_big_endian(r)
-        bytes_s = Utils.int_to_big_endian(s)
-
-        "\x30#{len}\x02#{len_r}#{bytes_r}\x02#{len_s}#{bytes_s}"
-      end
-
-      def encode_signature(v, r, s, base64=true)
-        bytes = "#{v.chr}#{Utils.zpad_int(r)}#{Utils.zpad_int(s)}"
-        base64 ? Base64.strict_encode64(bytes) : bytes
-      end
-
-      def decode_signature(sig, base64=true)
-        bytes = base64 ? Base64.strict_decode64(sig) : sig
-        [bytes[0].ord, Utils.big_endian_to_int(bytes[1,32]), Utils.big_endian_to_int(bytes[33,32])]
       end
     end
   end
