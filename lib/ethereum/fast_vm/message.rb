73f31539b23c76aca43116dcd75bbae06ac28011
Jan Xie <jan.h.xie@gmail.com>
2016-03-28 20:30:34 +0800

    add shard bounds to vm message

--- lib/ethereum/fast_vm/message.rb
+++ lib/ethereum/fast_vm/message.rb
@@ -5,18 +5,25 @@ module Ethereum
 
     class Message
 
-      attr_accessor :sender, :to, :value, :gas, :data, :depth, :logs, :code_address, :is_create
+      attr_accessor :sender, :to, :value, :gas, :data,
+        :left_bound, :right_bound, :depth, :logs, :code_address, :is_create
 
-      def initialize(sender, to, value, gas, data, depth:0, code_address:nil, is_create:false)
+      def initialize(sender, to, value, gas, data,
+                     left_bound: 0, right_bound: Constant::MAXSHARDS,
+                     depth:0, code_address:nil, is_create:false,
+                     transfers_value: true)
         @sender = sender
         @to = to
         @value = value
         @gas = gas
         @data = data
         @depth = depth
+        @left_bound = left_bound
+        @right_bound = right_bound
         @logs = []
         @code_address = code_address
         @is_create = is_create
+        @transfers_value = transfers_value
       end
 
       def to_s
