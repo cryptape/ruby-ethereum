a4f5b9f5c966d52c789609ba4fe6744a501bbf80
Jan Xie <jan.h.xie@gmail.com>
2016-03-28 16:38:52 +0800

    fast vm message and state (the same)

--- lib/ethereum/fast_vm.rb
+++ lib/ethereum/fast_vm.rb
@@ -1,6 +1,8 @@
 # -*- encoding : ascii-8bit -*-
 
 require 'ethereum/fast_vm/call_data'
+require 'ethereum/fast_vm/message'
+require 'ethereum/fast_vm/state'
 
 module Ethereum
   class VM
---
+++ lib/ethereum/fast_vm/message.rb
@@ -0,0 +1,29 @@
+# -*- encoding : ascii-8bit -*-
+
+module Ethereum
+  class FastVM
+
+    class Message
+
+      attr_accessor :sender, :to, :value, :gas, :data, :depth, :logs, :code_address, :is_create
+
+      def initialize(sender, to, value, gas, data, depth:0, code_address:nil, is_create:false)
+        @sender = sender
+        @to = to
+        @value = value
+        @gas = gas
+        @data = data
+        @depth = depth
+        @logs = []
+        @code_address = code_address
+        @is_create = is_create
+      end
+
+      def to_s
+        "#<#{self.class.name}:#{object_id} to=#{@to[0,8]}>"
+      end
+      alias :inspect :to_s
+    end
+
+  end
+end
