e15f399bba3cd627e404cfafe5f63ebabd8b9a5d
Jan Xie <jan.h.xie@gmail.com>
2016-05-06 22:13:23 +0800

    support LevelDB

---
+++ lib/ethereum/db/level_db.rb
@@ -0,0 +1,122 @@
+# -*- encoding : ascii-8bit -*-
+
+module Ethereum
+  module DB
+    class LevelDB < BaseDB
+
+      def initialize(dbfile)
+        logger.info "opening LevelDB", path: dbfile
+
+        @dbfile = dbfile
+        reopen
+
+        @commit_counter = 0
+        @uncommited = {}
+      end
+
+      def reopen
+        @db = LevelDB::DB.new @dbfile
+      end
+
+      def get(k)
+        logger.trace 'getting entry', key: Utils.encode_hex(k)[0,8]
+
+        if @uncommited.has_key?(k)
+          raise KeyError, 'key not in db' unless @uncommited[key]
+          logger.trace "from uncommited"
+          return @uncommited[k]
+        end
+
+        logger.trace "from db"
+        v = @db.get(k) or raise KeyError, k.inspect
+        o = decompress v
+        @uncommited[k] = o
+
+        o
+      end
+
+      def put(k, v)
+        logger.trace 'putting entry', key: Utils.encode_hex(k)[0,8], size: v.size
+        @uncommited[k] = v
+      end
+
+      def commit
+        logger.debug "committing", db: self
+
+        @db.batch do |b|
+          @uncommited.each do |k, v|
+            if v
+              b.put k, compress(v)
+            else
+              b.delete k
+            end
+          end
+        end
+        logger.debug 'committed', db: self, num: @uncommited.size
+        @uncommited = {}
+      end
+
+      def delete(k)
+        logger.trace 'deleting entry', key: key
+        @uncommited[k] = nil
+      end
+
+      def has_key?(k)
+        get(k)
+        true
+      rescue KeyError
+        false
+      end
+      alias :include? :has_key?
+
+      def ==(other)
+        other.instance_of?(self.class) && db == other.db
+      end
+
+      def to_s
+        "<DB at #{@db} uncommited=#{@uncommited.size}>"
+      end
+      alias inspect to_s
+
+      def inc_refcount(k, v)
+        put k, v
+      end
+
+      def dec_refcount(k)
+        # do nothing
+      end
+
+      def revert_refcount_changes(epoch)
+        # do nothing
+      end
+
+      def commit_refcount_changes(epoch)
+        # do nothing
+      end
+
+      def cleanup(epoch)
+        # do nothing
+      end
+
+      def put_temporarily(k, v)
+        inc_refcount(k, v)
+        dec_refcount(k)
+      end
+
+      private
+
+      def logger
+        @logger ||= Logger.new 'db.leveldb'
+      end
+
+      def decompress(x)
+        x
+      end
+
+      def compress(x)
+        x
+      end
+
+    end
+  end
+end
